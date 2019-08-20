92ce2d943e151576c743b308c1ab211be1d4e58a
Benjie Gillam <benjie@jemjie.com>
2019-08-20 13:13:35 +0100

    Restore rest of auto-mock

--- src/__mocks__/fsp.ts
+++ src/__mocks__/fsp.ts
@@ -3,3 +3,8 @@ import * as path from "path";
 export const readFile = jest.fn(
   async filepath => `[CONTENT:${path.relative(process.cwd(), filepath)}]`
 );
+
+const { writeFile, stat, readdir, mkdir, unlink } = jest.genMockFromModule(
+  "../fsp"
+);
+export { writeFile, stat, readdir, mkdir, unlink };
