44a88f3d0567989ac91f49e594acd563669b25dc
Benjie Gillam <benjie@jemjie.com>
2020-01-22 10:47:43 +0000

    Refactor current.ts

--- src/current.ts
+++ src/current.ts
@@ -1,203 +1,264 @@
 import * as fsp from "./fsp";
 import { isNoTransactionDefined } from "./header";
 import { ParsedSettings } from "./settings";
+import { Stats } from "fs";
+import * as assert from "assert";
 
-export interface CurrentOptions {
-  readBody: boolean;
-  splitBody?: boolean;
+const VALID_FILE_REGEX = /^([0-9]+)(-[-_a-zA-Z0-9]*)?\.sql$/;
+
+async function statOrNull(path: string): Promise<Stats | null> {
+  try {
+    return await fsp.stat(path);
+  } catch (e) {
+    if (e.code === "ENOENT") {
+      return null;
+    }
+    throw e;
+  }
 }
 
-export interface Current {
-  path: string;
+async function readFileOrNull(path: string): Promise<string | null> {
+  try {
+    return await fsp.readFile(path, "utf8");
+  } catch (e) {
+    if (e.code === "ENOENT") {
+      return null;
+    }
+    throw e;
+  }
+}
+async function readFileOrError(path: string): Promise<string> {
+  try {
+    return await fsp.readFile(path, "utf8");
+  } catch (e) {
+    throw new Error(`Failed to read file at '${path}': ${e.message}`);
+  }
+}
+
+export interface CurrentMigrationLocation {
   isFile: boolean;
+  path: string;
   exists: boolean;
-  body: string;
+  // stats: Stats,
 }
 
-const VALID_FILE_REGEX = /^([0-9]+)(-[-_a-z0-9]+)?\.sql$/g;
-const SPLIT_REGEX = /^--! split: ([0-9]+)(-[-_a-z0-9]+)?\.sql\n$/gm;
-const SPLIT = "--! split: {file}\n";
-const BLANK_MIGRATION_CONTENT = "-- Enter migration here\n";
-
-function getCurrentMigrationFilePath(parsedSettings: ParsedSettings): string {
-  return `${parsedSettings.migrationsFolder}/current.sql`;
-}
-
-function getCurrentMigrationDirectoryPath(
+export async function getCurrentMigrationLocation(
   parsedSettings: ParsedSettings
-): string {
-  return `${parsedSettings.migrationsFolder}/current`;
-}
+): Promise<CurrentMigrationLocation> {
+  const filePath = `${parsedSettings.migrationsFolder}/current.sql`;
+  const dirPath = `${parsedSettings.migrationsFolder}/current`;
 
-export async function getCurrent(
-  parsedSettings: ParsedSettings,
-  { readBody, splitBody = false }: CurrentOptions
-): Promise<Current> {
-  const filePath = getCurrentMigrationFilePath(parsedSettings);
-  const directoryPath = getCurrentMigrationDirectoryPath(parsedSettings);
+  const fileStats = await statOrNull(filePath);
+  const dirStats = await statOrNull(dirPath);
 
-  let fileExists: boolean;
-  try {
-    fileExists = (await fsp.stat(filePath)).isFile();
-  } catch (e) {
-    if (e.code !== "ENOENT") {
-      throw e;
-    }
-    fileExists = false;
+  if (fileStats && !fileStats.isFile()) {
+    throw new Error(`'${filePath}' exists but is not a file.`);
+  }
+  if (dirStats && !dirStats.isDirectory()) {
+    throw new Error(`'${dirPath}' exists but is not a directory.`);
   }
 
-  let directoryExists: boolean;
-  try {
-    directoryExists = (await fsp.stat(directoryPath)).isDirectory();
-  } catch (e) {
-    if (e.code !== "ENOENT") {
-      throw e;
-    }
-    directoryExists = false;
-  }
-
-  if (fileExists && directoryExists) {
+  if (fileStats && dirStats) {
     throw new Error(
-      "Invalid current migration. Both current.sql and current/ directory cannot co-exist at the same time."
+      `Invalid current migration: both the '${filePath}' file and the '${dirPath}' directory exist; only one of these may exist at a time.`
     );
   }
 
-  let path: string;
-  let isFile: boolean;
-  let exists: boolean;
-  let body: string;
-
-  if (fileExists || (!fileExists && !directoryExists)) {
-    // Defaults to file if neither file nor directory exist
-    path = filePath;
-    isFile = true;
-    exists = fileExists;
-
-    if (readBody) {
-      body = (await fsp.readFile(path, "utf8")).trim();
-    } else {
-      body = "";
-    }
-  } else {
-    path = directoryPath;
-    isFile = false;
-    const files = await fsp.readdir(directoryPath);
-    exists = files.length > 0;
-
-    if (exists && readBody) {
-      const parts = new Map<number, string>();
-      for (const file of files) {
-        const match = [...file.matchAll(VALID_FILE_REGEX)];
-        if (match.length != 1) {
-          throw new Error(
-            `Invalid current migration filename: ${file}. File must follow the naming 1.sql or 1-message.sql, where 1 is a unique number and message is optional.`
-          );
-        }
-
-        const matchResult = match[0];
-        const id = Number(matchResult[1]);
-        if (isNaN(id) || parts.has(id)) {
-          throw new Error(
-            `Invalid current migration filename: ${file}. File must start with a unique number.`
-          );
-        }
-
-        const filePath = `${path}/${file}`;
-        let body: string;
-        try {
-          body = (await fsp.readFile(filePath, "utf8")).trim();
-        } catch (e) {
-          throw new Error(
-            `Failed to read current migration file: ${filePath} (${e.message})`
-          );
-        }
-
-        if (isNoTransactionDefined(body) && files.length > 1) {
-          throw new Error(
-            "Cannot use --! no-transaction with multiple current migration files."
-          );
-        }
-
-        if (splitBody) {
-          const fileSplit = SPLIT.replace("{file}", file);
-          body = `${fileSplit}\n${body}`;
-        }
-
-        parts.set(id, body);
-      }
-
-      // Sort body parts
-      const ids = [...parts.keys()].sort((a, b) => a - b);
-
-      body = ids.map(id => parts.get(id)).join("\n\n") + "\n";
-    } else {
-      body = "";
-    }
-  }
+  const isFile = !dirStats;
+  const stats = isFile ? fileStats : dirStats;
+  const exists = !!stats;
 
   return {
-    path,
     isFile,
+    path: isFile ? filePath : dirPath,
     exists,
-    body,
+    // stats,
   };
 }
 
-export async function writeBlankCurrent(current: Current): Promise<void> {
-  if (current.isFile) {
-    await fsp.writeFile(current.path, BLANK_MIGRATION_CONTENT);
+function idFromFilename(file: string): number {
+  const matches = VALID_FILE_REGEX.exec(file);
+  if (!matches) {
+    throw new Error(
+      `Invalid current migration filename: '${file}'. File must follow the naming 1.sql or 1-message.sql, where 1 is a unique number and message is an optional alphanumeric string.`
+    );
+  }
+  const [, rawId, _message] = matches;
+  const id = parseInt(rawId, 10);
+
+  if (!id || !isFinite(id) || id < 1) {
+    throw new Error(
+      `Invalid current migration filename: '${file}'. File must start with a number.`
+    );
+  }
+  return id;
+}
+
+export async function readCurrentMigration(
+  _parsedSettings: ParsedSettings,
+  location: CurrentMigrationLocation
+): Promise<string> {
+  if (location.isFile) {
+    const content = await readFileOrNull(location.path);
+
+    // If file doesn't exist, treat it as if it were empty.
+    return content || "";
   } else {
-    const files = await fsp.readdir(current.path);
+    const files = await fsp.readdir(location.path);
+    const parts = new Map<
+      number,
+      {
+        file: string;
+        bodyPromise: Promise<string>;
+      }
+    >();
+
     for (const file of files) {
-      await fsp.unlink(`${current.path}/${file}`);
+      // Do not await during this loop, it will limit parallelism
+
+      if (!file.endsWith(".sql")) {
+        // Skip non-SQL files
+        continue;
+      }
+      const id = idFromFilename(file);
+      const other = parts.get(id);
+      if (other) {
+        throw new Error(
+          `Current migration filename clash: files must have a unique numeric prefix, but at least 2 files ('${file}' and '${other.file}') have the prefix '${id}'.`
+        );
+      }
+
+      const filePath = `${location.path}/${file}`;
+      const bodyPromise = readFileOrError(filePath);
+
+      parts.set(id, {
+        file,
+        bodyPromise,
+      });
     }
-    await fsp.writeFile(`${current.path}/1.sql`, BLANK_MIGRATION_CONTENT);
+
+    const ids = [...parts.keys()].sort((a, b) => a - b);
+    let wholeBody = "";
+    for (const id of ids) {
+      const { file, bodyPromise } = parts.get(id)!;
+      const body = await bodyPromise;
+      if (isNoTransactionDefined(body) && ids.length > 1) {
+        throw new Error(
+          `Error in '${location.path}/${file}': cannot use '--! no-transaction' with multiple current migration files.`
+        );
+      }
+      if (wholeBody.length > 0) {
+        wholeBody += "\n";
+      }
+      wholeBody += `--! split: ${file}\n`;
+      wholeBody += body;
+    }
+    return wholeBody;
   }
 }
 
-export async function writeCurrentFromCommit(
-  parsedSettings: ParsedSettings,
+export async function writeCurrentMigration(
+  _parsedSettings: ParsedSettings,
+  location: CurrentMigrationLocation,
   body: string
 ): Promise<void> {
-  const parts: Array<RegExpMatchArray> = [...body.matchAll(SPLIT_REGEX)];
-  if (parts.length == 0) {
-    await fsp.writeFile(getCurrentMigrationFilePath(parsedSettings), body);
+  if (location.isFile) {
+    await fsp.writeFile(location.path, body);
   } else {
-    const directory = getCurrentMigrationDirectoryPath(parsedSettings);
+    // Split body and write to files
 
-    try {
-      const files = await fsp.readdir(directory);
-      for (const file of files) {
-        await fsp.unlink(`${directory}/${file}`);
+    const lines = body.split("\n");
+
+    /**
+     * List of filenames we've written, so we can determine which files not to
+     * delete.
+     */
+    const filenamesWritten: string[] = [];
+
+    /**
+     * List of write operation promises, so we can do all our waiting in
+     * parallel at the end.
+     */
+    const writePromises: Array<Promise<void>> = [];
+
+    /**
+     * The next file that will be written to, once all lines are accumulated.
+     */
+    let nextFileToWrite: string | null = null;
+
+    /**
+     * The lines being accumulated to write to `nextFileToWrite`.
+     */
+    let linesToWrite: string[] = [];
+
+    /**
+     * The highest file index we've seen, so that we can ensure that we don't
+     * have any ambiguities or conflicts.
+     */
+    let highestIndex = 0;
+
+    /**
+     * Writes `linesToWrite` to `nextFileToWrite` (or '1.sql' if unknown), then
+     * resets these variables ready for the next batch.
+     */
+    const flushToFile = (): void => {
+      if (!linesToWrite.length && !nextFileToWrite) {
+        // Optimisation to avoid writing the initial empty migration file before the first `--! split`
+        return;
       }
-    } catch (e) {
-      if (e.code !== "ENOENT") {
-        throw e;
+      const body = linesToWrite.join("\n");
+      const fileName = nextFileToWrite || "1.sql";
+      const id = idFromFilename(fileName);
+      if (id <= highestIndex) {
+        throw new Error(
+          `Bad migration, split ids must be monotonically increasing, but '${id}' (from '${fileName}') <= '${highestIndex}'.`
+        );
+      }
+      highestIndex = id;
+
+      writePromises.push(fsp.writeFile(`${location.path}/${fileName}`, body));
+      filenamesWritten.push(fileName);
+
+      linesToWrite = [];
+      nextFileToWrite = null;
+    };
+
+    for (const line of lines) {
+      // Do not await in this loop, it decreases parallelism
+
+      const matches = /^--! split: ([0-9]+(?:-[-_a-z0-9]+)?\.sql)$/.exec(line);
+      if (matches) {
+        // Write out previous linesToWrite, if appropriate
+        flushToFile();
+
+        // Prepare to write next linesToWrite
+        nextFileToWrite = matches[1];
+      } else {
+        // Regular line, just add to next linesToWrite
+        linesToWrite.push(line);
       }
     }
 
-    for (let i = 0; i < parts.length; ++i) {
-      const part = parts[i];
-      if (part.index == undefined) {
-        // Not possible but required for compiler and lint validation
-        continue;
-      }
+    // Handle any trailing lines
+    flushToFile();
 
-      const name =
-        part
-          .slice(1, part.length)
-          .filter((it: string | undefined) => it != undefined)
-          .join("") + ".sql";
+    if (writePromises.length === 0) {
+      // Body must have been empty, so no files were written.
+      assert.equal(body.length, 0);
 
-      const bodyPart =
-        body
-          .substring(
-            part.index + part[0].length,
-            i < parts.length - 1 ? parts[i + 1].index : body.length
-          )
-          .trim() + "\n";
-
-      fsp.writeFile(`${directory}/${name}`, bodyPart);
+      // Lets write out just the one empty file.
+      writePromises.push(fsp.writeFile(`${location.path}/1.sql`, ""));
     }
+
+    // Clear out old files that were not overwritten
+    const files = await fsp.readdir(location.path);
+    for (const file of files) {
+      if (VALID_FILE_REGEX.test(file) && !filenamesWritten.includes(file)) {
+        writePromises.push(fsp.unlink(`${location.path}/${file}`));
+      }
+    }
+
+    // Wait for writing to finish
+    await Promise.all(writePromises);
   }
 }
