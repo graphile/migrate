ee13ddbf6249d931303e8674a067ef12cfcc8dc4
Benjie Gillam <benjie@jemjie.com>
2020-03-04 15:52:36 +0000

    Enhance isNoTransactionDefined check

--- src/header.ts
+++ src/header.ts
@@ -1,5 +1,5 @@
 export function isNoTransactionDefined(sql: string): boolean {
   const i = sql.indexOf("\n");
-  const firstLine = sql.substring(0, i);
-  return /^--!\s*no-transaction\b/.exec(firstLine) != null;
+  const firstLine = i > 0 ? sql.substring(0, i) : sql;
+  return /^--!\s*no-transaction\b/.test(firstLine);
 }
