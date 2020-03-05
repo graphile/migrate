c7883fe34557367413648fd91f0cc125a54d0e07
Benjie Gillam <benjie@jemjie.com>
2020-03-05 12:55:47 +0000

    Test committing with commit messages

--- __tests__/commit.test.ts
+++ __tests__/commit.test.ts
@@ -5,37 +5,8 @@ import { promises as fsp } from "fs";
 import * as mockFs from "mock-fs";
 
 import { commit } from "../src";
+import { sluggify } from "../src/sluggify";
 import { TEST_DATABASE_URL, TEST_SHADOW_DATABASE_URL } from "./helpers";
-
-const MIGRATION_1_TEXT = "create table foo (id serial primary key);";
-const MIGRATION_1_HASH = "bfe32129112f19d4cadd717c1c15ed7ccbca4408";
-const MIGRATION_1_COMMITTED = `--! Previous: -\n--! Hash: sha1:${MIGRATION_1_HASH}\n\n${MIGRATION_1_TEXT.trim()}\n`;
-
-const MIGRATION_2_TEXT =
-  "\n\n\ncreate table bar (id serial primary key);\n\n\n";
-const MIGRATION_2_HASH = createHash("sha1")
-  .update(`sha1:${MIGRATION_1_HASH}\n${MIGRATION_2_TEXT.trim()}` + "\n")
-  .digest("hex");
-const MIGRATION_2_COMMITTED = `--! Previous: sha1:${MIGRATION_1_HASH}\n--! Hash: sha1:${MIGRATION_2_HASH}\n\n${MIGRATION_2_TEXT.trim()}\n`;
-
-const MIGRATION_MULTIFILE_FILES = {
-  "001.sql": "select 1;",
-  "002-two.sql": "select 2;",
-  "003.sql": "select 3;",
-};
-
-const MIGRATION_MULTIFILE_TEXT = `\
---! split: 001.sql
-select 1;
---! split: 002-two.sql
-select 2;
---! split: 003.sql
-select 3;
-`;
-const MIGRATION_MULTIFILE_HASH = createHash("sha1")
-  .update(`sha1:${MIGRATION_1_HASH}\n${MIGRATION_MULTIFILE_TEXT.trim()}` + "\n")
-  .digest("hex");
-const MIGRATION_MULTIFILE_COMMITTED = `--! Previous: sha1:${MIGRATION_1_HASH}\n--! Hash: sha1:${MIGRATION_MULTIFILE_HASH}\n\n${MIGRATION_MULTIFILE_TEXT.trim()}\n`;
 
 beforeEach(async () => {
   mockFs({ migrations: mockFs.directory() });
@@ -44,41 +15,8 @@ afterEach(() => {
   mockFs.restore();
 });
 
-it("can commit the first migration", async () => {
-  mockFs({
-    "migrations/current.sql": MIGRATION_1_TEXT,
-  });
-
-  await commit({
-    connectionString: TEST_DATABASE_URL,
-    shadowConnectionString: TEST_SHADOW_DATABASE_URL,
-  });
-  expect(await fsp.readFile("migrations/committed/000001.sql", "utf8")).toEqual(
-    MIGRATION_1_COMMITTED,
-  );
-});
-
-it("can commit the second migration", async () => {
-  mockFs({
-    "migrations/committed/000001.sql": MIGRATION_1_COMMITTED,
-    "migrations/current.sql": MIGRATION_2_TEXT,
-  });
-
-  await commit({
-    connectionString: TEST_DATABASE_URL,
-    shadowConnectionString: TEST_SHADOW_DATABASE_URL,
-  });
-  expect(await fsp.readFile("migrations/committed/000001.sql", "utf8")).toEqual(
-    MIGRATION_1_COMMITTED,
-  );
-  expect(await fsp.readFile("migrations/committed/000002.sql", "utf8")).toEqual(
-    MIGRATION_2_COMMITTED,
-  );
-});
-
 it("aborts if current.sql is empty", async () => {
   mockFs({
-    "migrations/committed/000001.sql": MIGRATION_1_COMMITTED,
     "migrations/current.sql": "-- JUST A COMMENT\n",
   });
 
@@ -94,20 +32,122 @@ it("aborts if current.sql is empty", async () => {
   );
 });
 
-it("can commit multi-file migration", async () => {
-  mockFs({
-    "migrations/committed/000001.sql": MIGRATION_1_COMMITTED,
-    "migrations/current": MIGRATION_MULTIFILE_FILES,
-  });
+describe.each([[undefined], ["My Commit Message"]])(
+  "commit message",
+  commitMessage => {
+    const commitMessageSlug = commitMessage
+      ? `-${sluggify(commitMessage)}`
+      : ``;
+    const MIGRATION_1_TEXT = "create table foo (id serial primary key);";
+    const MIGRATION_1_HASH = "bfe32129112f19d4cadd717c1c15ed7ccbca4408";
+    const MIGRATION_1_COMMITTED = `--! Previous: -\n--! Hash: sha1:${MIGRATION_1_HASH}${
+      commitMessage ? `\n--! Message: ${commitMessage}` : ``
+    }\n\n${MIGRATION_1_TEXT.trim()}\n`;
 
-  await commit({
-    connectionString: TEST_DATABASE_URL,
-    shadowConnectionString: TEST_SHADOW_DATABASE_URL,
-  });
-  expect(await fsp.readFile("migrations/committed/000001.sql", "utf8")).toEqual(
-    MIGRATION_1_COMMITTED,
-  );
-  expect(await fsp.readFile("migrations/committed/000002.sql", "utf8")).toEqual(
-    MIGRATION_MULTIFILE_COMMITTED,
-  );
-});
+    const MIGRATION_2_TEXT =
+      "\n\n\ncreate table bar (id serial primary key);\n\n\n";
+    const MIGRATION_2_HASH = createHash("sha1")
+      .update(`sha1:${MIGRATION_1_HASH}\n${MIGRATION_2_TEXT.trim()}` + "\n")
+      .digest("hex");
+    const MIGRATION_2_COMMITTED = `--! Previous: sha1:${MIGRATION_1_HASH}\n--! Hash: sha1:${MIGRATION_2_HASH}${
+      commitMessage ? `\n--! Message: ${commitMessage}` : ``
+    }\n\n${MIGRATION_2_TEXT.trim()}\n`;
+
+    const MIGRATION_MULTIFILE_FILES = {
+      "001.sql": "select 1;",
+      "002-two.sql": "select 2;",
+      "003.sql": "select 3;",
+    };
+
+    const MIGRATION_MULTIFILE_TEXT = `\
+--! split: 001.sql
+select 1;
+--! split: 002-two.sql
+select 2;
+--! split: 003.sql
+select 3;
+`;
+    const MIGRATION_MULTIFILE_HASH = createHash("sha1")
+      .update(
+        `sha1:${MIGRATION_1_HASH}\n${MIGRATION_MULTIFILE_TEXT.trim()}` + "\n",
+      )
+      .digest("hex");
+    const MIGRATION_MULTIFILE_COMMITTED = `--! Previous: sha1:${MIGRATION_1_HASH}\n--! Hash: sha1:${MIGRATION_MULTIFILE_HASH}${
+      commitMessage ? `\n--! Message: ${commitMessage}` : ``
+    }\n\n${MIGRATION_MULTIFILE_TEXT.trim()}\n`;
+
+    it("can commit the first migration", async () => {
+      mockFs({
+        "migrations/current.sql": MIGRATION_1_TEXT,
+      });
+
+      await commit(
+        {
+          connectionString: TEST_DATABASE_URL,
+          shadowConnectionString: TEST_SHADOW_DATABASE_URL,
+        },
+        commitMessage,
+      );
+      expect(
+        await fsp.readFile(
+          `migrations/committed/000001${commitMessageSlug}.sql`,
+          "utf8",
+        ),
+      ).toEqual(MIGRATION_1_COMMITTED);
+    });
+
+    it("can commit the second migration", async () => {
+      mockFs({
+        [`migrations/committed/000001${commitMessageSlug}.sql`]: MIGRATION_1_COMMITTED,
+        "migrations/current.sql": MIGRATION_2_TEXT,
+      });
+
+      await commit(
+        {
+          connectionString: TEST_DATABASE_URL,
+          shadowConnectionString: TEST_SHADOW_DATABASE_URL,
+        },
+        commitMessage,
+      );
+      expect(
+        await fsp.readFile(
+          `migrations/committed/000001${commitMessageSlug}.sql`,
+          "utf8",
+        ),
+      ).toEqual(MIGRATION_1_COMMITTED);
+      expect(
+        await fsp.readFile(
+          `migrations/committed/000002${commitMessageSlug}.sql`,
+          "utf8",
+        ),
+      ).toEqual(MIGRATION_2_COMMITTED);
+    });
+
+    it("can commit multi-file migration", async () => {
+      mockFs({
+        [`migrations/committed/000001${commitMessageSlug}.sql`]: MIGRATION_1_COMMITTED,
+        "migrations/current": MIGRATION_MULTIFILE_FILES,
+      });
+
+      await commit(
+        {
+          connectionString: TEST_DATABASE_URL,
+          shadowConnectionString: TEST_SHADOW_DATABASE_URL,
+        },
+        commitMessage,
+      );
+      expect(
+        await fsp.readFile(
+          `migrations/committed/000001${commitMessageSlug}.sql`,
+          "utf8",
+        ),
+      ).toEqual(MIGRATION_1_COMMITTED);
+      expect(
+        await fsp.readFile(
+          `migrations/committed/000002${commitMessageSlug}.sql`,
+          "utf8",
+        ),
+      ).toEqual(MIGRATION_MULTIFILE_COMMITTED);
+    });
+  },
+);
