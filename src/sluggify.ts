31aafad23a61a09934c696f8340d41c3a4a3b999
Benjie Gillam <benjie@jemjie.com>
2020-03-05 10:33:23 +0000

    Allow underscores, limit length

--- src/sluggify.ts
+++ src/sluggify.ts
@@ -1,7 +1,8 @@
 export function sluggify(text: string): string {
   return text
     .toLowerCase()
-    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/[^a-z0-9_]+/g, "-")
     .replace(/--+/g, "-")
-    .replace(/(^-+|-+$)/g, "");
+    .replace(/(^-+|-+$)/g, "")
+    .substr(0, 60);
 }
