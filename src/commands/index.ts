21dce93b37bf99e552dd8cead563522c5ff7e3f6
Benjie Gillam <benjie@jemjie.com>
2020-03-05 11:47:19 +0000

    Expose uncommit command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,4 +2,5 @@ export { migrate } from "./migrate";
 export { watch } from "./watch";
 export { reset } from "./reset";
 export { commit } from "./commit";
+export { uncommit } from "./uncommit";
 export { status } from "./status";
