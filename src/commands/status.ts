bed365aedca9e64a618d8664637edf9cee4437d3
Benjie Gillam <benjie@jemjie.com>
2020-03-17 14:32:07 +0000

    feat: status without database connection (#61)
    * feat: status without database connection
    
    * Update docs
    
    * Aliased type

--- src/commands/status.ts
+++ src/commands/status.ts
@@ -8,61 +8,90 @@ import { ParsedSettings, parseSettings, Settings } from "../settings";
 import { getSettings } from "./_common";
 
 interface Status {
-  remainingMigrations: Array<string>;
+  remainingMigrations?: Array<string>;
   hasCurrentMigration: boolean;
 }
 
-async function _status(parsedSettings: ParsedSettings): Promise<Status> {
-  const connectionString = parsedSettings.connectionString;
-  if (!connectionString) {
-    throw new Error("Could not determine connection string");
+interface StatusOptions {
+  skipDatabase?: boolean;
+}
+
+async function _status(
+  parsedSettings: ParsedSettings,
+  { skipDatabase }: StatusOptions,
+): Promise<Status> {
+  // Checks that don't need a database connection
+  const currentLocation = await getCurrentMigrationLocation(parsedSettings);
+  const body = await readCurrentMigration(parsedSettings, currentLocation);
+  const minifiedBody = pgMinify(body);
+  const hasCurrentMigration = minifiedBody !== "";
+
+  // Checks that need a database connection
+  let remainingMigrations: undefined | string[];
+  if (!skipDatabase) {
+    const connectionString = parsedSettings.connectionString;
+    if (!connectionString) {
+      throw new Error("Could not determine connection string");
+    }
+    await withClient(connectionString, parsedSettings, async pgClient => {
+      const lastMigration = await getLastMigration(pgClient, parsedSettings);
+      const remainingMigrationDefinitions = await getMigrationsAfter(
+        parsedSettings,
+        lastMigration,
+      );
+      remainingMigrations = remainingMigrationDefinitions.map(m => m.filename);
+      return {
+        remainingMigrations,
+      };
+    });
   }
-  return withClient(connectionString, parsedSettings, async pgClient => {
-    const lastMigration = await getLastMigration(pgClient, parsedSettings);
-    const remainingMigrations = await getMigrationsAfter(
-      parsedSettings,
-      lastMigration,
-    );
-    const currentLocation = await getCurrentMigrationLocation(parsedSettings);
-    const body = await readCurrentMigration(parsedSettings, currentLocation);
-    const minifiedBody = pgMinify(body);
-    const hasCurrentMigration = minifiedBody !== "";
-    return {
-      remainingMigrations: remainingMigrations.map(m => m.filename),
-      hasCurrentMigration,
-    };
-  });
+
+  return {
+    remainingMigrations,
+    hasCurrentMigration,
+  };
 }
 
-export async function status(settings: Settings): Promise<Status> {
+export async function status(
+  settings: Settings,
+  options: StatusOptions = {},
+): Promise<Status> {
   const parsedSettings = await parseSettings(settings, true);
-  return _status(parsedSettings);
+  return _status(parsedSettings, options);
 }
 
-export const statusCommand: CommandModule<never, {}> = {
+export const statusCommand: CommandModule<never, StatusOptions> = {
   command: "status",
   aliases: [],
   describe: `\
 Exits with a bitmap status code indicating statuses:
 
-- 1 if there are committed migrations that have not been executed yet
+- 1 if there are committed migrations that have not been executed yet (requires DB connection)
 - 2 if the current migration is non-empty (ignoring comments)
 
 If both of the above are true then the output status will be 3 (1+2). If neither
 are true, exit status will be 0 (success). Additional messages may also be output.`,
-  builder: {},
-  handler: async () => {
+  builder: {
+    skipDatabase: {
+      type: "boolean",
+      description: "Skip checks that require a database connection.",
+      default: false,
+    },
+  },
+  handler: async argv => {
     /* eslint-disable no-console */
     let exitCode = 0;
-    const details = await status(await getSettings());
-    const remainingCount = details.remainingMigrations.length;
-    if (remainingCount) {
-      console.log(
-        `There are ${remainingCount} committed migrations pending:\n\n  ${details.remainingMigrations.join(
-          "\n  ",
-        )}`,
-      );
-      exitCode += 1;
+    const details = await status(await getSettings(), argv);
+    if (details.remainingMigrations) {
+      const remainingCount = details.remainingMigrations?.length;
+      if (remainingCount > 0) {
+        console.log(
+          `There are ${remainingCount} committed migrations pending:\n\n  ${details.remainingMigrations.join(
+            "\n  ",
+          )}`,
+        );
+        exitCode += 1;
+      }
     }
     if (details.hasCurrentMigration) {
       if (exitCode) {
