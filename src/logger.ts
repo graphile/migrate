fa7f988bc5fabeb96bdea0deee43a869160e565d
Sergey Trusov <Trusov@axinom.com>
2021-04-21 09:22:41 +0300

    use default console log factory

--- src/logger.ts
+++ src/logger.ts
@@ -1,30 +1,17 @@
-import { LogFunctionFactory, Logger, LogLevel } from "@graphile/logger";
+import { Logger, makeConsoleLogFactory } from "@graphile/logger";
 
 export { LogLevel as MigrateLogLevel } from "@graphile/logger";
 export { Logger as MigrateLogger } from "@graphile/logger";
-export type MigrateLogFactory = LogFunctionFactory<Record<string, unknown>>;
+export { LogFunctionFactory as MigrateLogFactory } from "@graphile/logger";
 export interface MigrateLogMeta {
   error?: Error;
 }
 
-const migrateLogFactory: MigrateLogFactory = () => {
-  return (level: LogLevel, message: string): void => {
-    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-    const method = (() => {
-      switch (level) {
-        case LogLevel.ERROR:
-          return "error" as const;
-        case LogLevel.WARNING:
-          return "warn" as const;
-        case LogLevel.INFO:
-        default:
-          return "log" as const;
-      }
-    })();
-
-    // eslint-disable-next-line no-console
-    console[method](message);
-  };
-};
-
-export const defaultMigrateLogger = new Logger(migrateLogFactory);
+export const defaultMigrateLogger = new Logger(
+  makeConsoleLogFactory({
+    format: `%s`,
+    formatParameters(_level, message) {
+      return [message];
+    },
+  }),
+);
