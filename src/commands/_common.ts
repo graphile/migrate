ed5480558ff83f0cda9aaefa66a023ac743a02c8
Benjie Gillam <benjie@jemjie.com>
2024-01-26 15:16:34 +0000

    Deal with edge cases

--- src/commands/_common.ts
+++ src/commands/_common.ts
@@ -77,8 +77,8 @@ export async function getSettings(options: Options = {}): Promise<Settings> {
     const relativePath = pathToFileURL(resolve(process.cwd(), path)).href;
 
     try {
-      const module = await import(relativePath);
-      return module.default as Settings;
+      const module = (await import(relativePath)) as Record<string, unknown>;
+      return (module.default ?? module) as Settings;
     } catch (e) {
       throw new Error(
         `Failed to import '${relativePath}'; error:\n    ${
